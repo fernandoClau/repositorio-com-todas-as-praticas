b5f8fb074b1915d4d7c75fe7d15146626cbdf456
Fernando Cauan <123105182+fernandoClau@users.noreply.github.com>
2023-03-03 19:22:49 -0300

    Update Questao_1_Struct.c

--- Questao_1_Struct.c
+++ Questao_1_Struct.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+/* nessa parte fasso a implementacao da estrutura struct e logo enseguida renomeio*/
 typedef struct funcionario{
 	char nome[50];
 	float salario;
 	int identificador; 
 	char cargo[50];
 }Funcionario;
-
+/* aqui fasso uma fuçao para que o usuario possa preencher os campos de acordo com o que é pedido */
 void preenche(Funcionario *campo, int n){
     int i;
     for (i = 0; i < n; i++) {
@@ -20,7 +20,8 @@ void preenche(Funcionario *campo, int n){
 		printf("informe seu cargo: ");
 	scanf(" %[^\n]s", &campo[i].cargo); 
 }
-}
+}  
+/* aqui fasso um outra funçao onde vai analisa cada salario informado e compara qual é o maior e menor salario*/
   void maiormenorsalario(Funcionario *campo, int n){
     int i; 
     float maiorsalario=0, menorsalario=0, aux=0;
@@ -39,6 +40,7 @@ char maiors, menors;
     printf("o maior salario: %f\t cargo: %c\n", maiorsalario, maiors);
     printf("o menor salario: %f\t cargo: %c", menorsalario, menors);
   } 
+/*nesse trecho vai imprimir para usuario as informaçoes que ele digitou */
 void imprima(Funcionario *campo, int n){
     int i;
     for (i = 0; i < n; i++) {
@@ -46,12 +48,15 @@ void imprima(Funcionario *campo, int n){
 	printf("%s\t %d\t %f\t %s\n", campo[i].nome, campo[i].identificador, campo[i].salario, campo[i].cargo);
 	}
 }
+/*nessa fuçao permite que o usuario possa altera seu salario se ele desejar */
 void altera(Funcionario * campo){
  float novo_salario;
  printf("informe seu novo salario: ");
  scanf("%f", &novo_salario);
  campo->salario=novo_salario;
  }
+
+/*na funcao principal fasso a chamada de todas as funçoes e pergunto ao usuario se ele deseja altera seu salario*/
 int main(void){
 int c;
   Funcionario pessoa;
